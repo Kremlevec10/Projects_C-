24855eb87dcc0522f0b5833218b37d142d890236
Ruslan <Ruslan90.0843@gmail.com>
2022-11-27 14:54:41 +0400

    Приветствие программы по имени и фамилии

---
+++ project1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите свое Имя");
+string userFirstName = Console.ReadLine();
+Console.WriteLine("Введите свою Фамилию");
+string userSecondName = Console.ReadLine();
+Console.WriteLine("Здраствуйте Наш дорогой " + userFirstName + " " + userSecondName);
+
+
