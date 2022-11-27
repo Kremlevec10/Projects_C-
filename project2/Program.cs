9c968fa17dbfe95cd46b5ddfab06e03b53274b10
Ruslan <Ruslan90.0843@gmail.com>
2022-11-27 15:09:48 +0400

    Добавил код по считыванию первого числа

---
+++ project2/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// Задача ввести с консоли 2 числа и найти их сумму?
+
+Console.Write("Введите  первое число");
+int ferstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ferstNumber);
+
+
+
